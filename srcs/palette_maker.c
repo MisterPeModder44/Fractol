e76dd4e83195b61e074c1fa45f669c586f0e4427
Yanis GUAYE <yguaye@student.42.fr>
2018-01-22 08:56:12 +0100

    99% working palette

--- srcs/palette_maker.c
+++ srcs/palette_maker.c
@@ -6,7 +6,7 @@
 /*   By: yguaye <yguaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/20 17:09:08 by yguaye            #+#    #+#             */
-/*   Updated: 2018/01/20 19:37:42 by yguaye           ###   ########.fr       */
+/*   Updated: 2018/01/22 08:49:09 by yguaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,25 +65,40 @@ static t_palette	*unicolor_palette(t_palette *pal, t_color_point *color)
 	return (pal);
 }
 
+int8_t			interpolate(uint8_t c1, uint8_t c2, double hue)
+{
+	if (c2 < c1)
+		return ((int8_t)(-((int32_t)((c1 - c2) * hue + c2)) + 255));
+	return ((c2 - c1) * hue + c1);
+}
 static void			make_gradient(t_palette *pal, t_color_point *p1,
 		t_color_point *p2)
 {
+	uint32_t		pos1;
+	uint32_t		pos2;
 	uint32_t		i;
-	uint32_t		end;
 	double			size;
 
-	i = SCALE_POS(p1->pos, pal->max_hue, pal->size);
-	end = SCALE_POS(p2->pos, pal->max_hue, pal->size);
-	size = end - i;
-	printf("==\nsize: %.1f\n", size);
+	i = 0;
+	pos1 = (uint32_t)((double)(pal->size * p1->pos) / (double)pal->max_hue);
+	pos2 = (uint32_t)((double)(pal->size * p2->pos) / (double)pal->max_hue);
+	size = pos2 - pos1;
+	printf("==\nsize: %.1f\ni: %u\n", size, i);
 	printf("color 1: 0x%.2hhx%.2hhx%.2hhx\n", p1->r, p1->g, p1->b);
 	printf("color 2: 0x%.2hhx%.2hhx%.2hhx\n", p2->r, p2->g, p2->b);
-	while (i < end)
+	//	if (p1->pos != 0)
+	//		--i;
+	while (i + pos1 < pos2)
 	{
-		pal->reds[i] = (p2->r - p1->r) * ((double)i / size) + p1->r;
-		pal->greens[i] = (p2->g - p1->g) * ((double)i / size) + p1->g;
-		pal->blues[i] = (p2->b - p1->b) * ((double)i / size) + p1->b;
-		printf("color at %u: 0x%.2hhx%.2hhx%.2hhx\n", i, pal->reds[i], pal->greens[i], pal->blues[i]);
+		pal->reds[i + pos1] = interpolate(p1->r, p2->r, ((double)i / size));
+		pal->blues[i + pos1] = interpolate(p1->g, p2->g, ((double)i / size));
+		pal->greens[i + pos1] = interpolate(p1->b, p2->b, ((double)i / size));
+		//pal->greens[i] = interpolate(p1->g, p2->g, ((double)i / size));
+		//pal->blues[i] = interpolate(p1->b, p2->b, ((double)i / size));
+		//pal->reds[i] = (p2->r - p1->r) * ((double)i / size) + p1->r;
+		//pal->greens[i] = (p2->g - p1->g) * ((double)i / size) + p1->g;
+		//pal->blues[i] = (p2->b - p1->b) * ((double)i / size) + p1->b;
+		printf("color at %u: 0x%.2hhx%.2hhx%.2hhx\n", i + pos1, pal->reds[i], pal->greens[i], pal->blues[i]);
 		++i;
 	}
 }
