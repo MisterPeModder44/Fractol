36d68697b170c37985739668f45bc150bb530e96
Yanis GUAYE <yguaye@student.42.fr>
2018-02-28 10:59:58 +0100

    Add signal handling

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,20 @@
 /*   By: yguaye <yguaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/18 08:32:01 by yguaye            #+#    #+#             */
-/*   Updated: 2018/02/27 16:15:26 by yguaye           ###   ########.fr       */
+/*   Updated: 2018/02/28 10:44:50 by yguaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
+#include <signal.h>
+#include <libft_base/io.h>
 #include "fractol.h"
+
+static void		fractol_signal_handler(int sig)
+{
+	quit_fractol(NULL, "\b\bfractol: cought interruption signal, exiting...");
+	(void)sig;
+}
 
 int				main(int ac, char **av)
 {
@@ -20,6 +28,9 @@ int				main(int ac, char **av)
 	t_fractal		frac;
 	t_args			*args;
 
+	if (signal(SIGINT, &fractol_signal_handler) == SIG_ERR)
+		ft_putendl_fd("fractol: \033[1;93mwarning:\033[0m couldn't catch "
+				"interuption signal!", 2);
 	init_mlx_context(&ctx, &cl_ctx);
 	args = add_fractol_args(&ctx);
 	if (parse_args(args, ac, av) && has_arg_errors(args, ARG_EMPTY))
